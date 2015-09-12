2c9cb3afaae93a49bb0a6bdf487849c1a344eee7
Alex Yu <AlexYu@587234002.guest.umich.net>
2015-09-12 07:23:27 -0400

    geocode test 1

--- geocoding.js
+++ geocoding.js
@@ -1,2 +1,17 @@
-var 
-function geocodeLocation()
+var geocoder;
+
+function geocodeLocation(place, map) {
+	geocoder = new google.maps.Geocoder();
+	var address = place;
+	geocoder.geocode({'address':address}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			var marker = new google.maps.Marker({
+        		map: map,
+        		position: results[0].geometry.location
+      		});
+      } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
