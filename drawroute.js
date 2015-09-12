173cfcc511ca9d1b6aafabe0972595f05f426ba0
angelaqiu <angelaxqiu@gmail.com>
2015-09-12 08:27:07 -0400

    idk

--- drawroute.js
+++ drawroute.js
@@ -9,13 +9,22 @@ function drawRoute() {
 
     directionsDisplay.setMap(map);
 
-    var start = new google.maps.LatLng(18.210885, -67.140884);
-    var end = new google.maps.LatLng(18.211685, -67.141684);
+    var point1 = new google.maps.LatLng(-33.8975098545041,151.09962701797485);
+    var point2 = new google.maps.LatLng(-33.8584421519279,151.0693073272705);
+    var point3 = new google.maps.LatLng(-33.84525521656404,151.0421848297119);
+
+    var wps = [{ location: point1 }, { location: point2 }, {location: point3}];
+
+    var org = new google.maps.LatLng ( -33.89192157947345,151.13604068756104);
+    var dest = new google.maps.LatLng ( -33.69727974097957,150.29047966003418);
+
     var request = {
-    origin: start,
-    destination: end,
+    origin: org,
+    destination: dest,
+    waypoints: wps,
     travelMode: google.maps.TravelMode.WALKING
     };
+    
     directionsService.route(request, function(result, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(result);
