dd648342175dae3ec811467da8fae92d30e18f02
angelaqiu <angelaxqiu@gmail.com>
2015-09-12 07:52:38 -0400

    readding script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+var map;
+var infowindow;
+
+function initMap() {
+  var pyrmont = {lat: 40.4483, lng: -79.9461};
+
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15
+  });
+
+  infowindow = new google.maps.InfoWindow();
+
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 500,
+    types: ['store']
+  }, callback);
+
+  listUpcomingEvents();
+  geocodeLocation("Stever House");
+  drawRoute();
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
